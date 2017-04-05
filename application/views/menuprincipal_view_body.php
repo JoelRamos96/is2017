3c08bb6c0e8d60538afb2c16e15394683a95f9e1
joel alejandro ramos avalos <joel alejandro ramos avalos>
2017-04-05 08:13:06 -0500

    proyecto

---
+++ application/views/menuprincipal_view_body.php
@@ -0,0 +1,27 @@
+
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">CRUD Trabajadores</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="index.php/Crud_controller/agregar_Trabajador"><span class="glyphicon glyphicon-plus-sign"></span>&nbsp;Agregar</a></li>
+            <li><a href="index.php/Crud_controller/cargaVistaConsulta"><span class="glyphicon glyphicon-search"></span>&nbsp;Buscar</a></li>
+            <li><a href="index.php/Crud_controller/cargarVistaCambiar"><span class="glyphicon glyphicon-pencil"></span>&nbsp;Cambiar</a></li>
+            <li><a href="index.php/Crud_controller/cargarVistaBorrar"><span class="glyphicon glyphicon-remove-sign"></span>&nbsp;Eliminar</a></li>
+            <li><a href="index.php/Crud_controller/reporteTrabajadores"><span class="glyphicon glyphicon-list-alt"></span>&nbsp;Reportes</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+
+
